9a088d23bd48afce06cc5f8402434ba1b6ddbd4a
Gabriel <gabrielcavalheiro20@gmail.com>
2023-09-12 20:23:39 -0300

    teste quase terminando a atv

--- apiPastelaria.py
+++ apiPastelaria.py
@@ -14,6 +14,10 @@ app.include_router(FuncionarioDAO.router)
 app.include_router(ClienteDAO.router)
 app.include_router(ProdutoDAO.router)
 
+#cria, caso não existam, as tabelas de todos os modelos importados
+import db
+db.criaTabelas()
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run('apiPastelaria:app', host=HOST, port=int(PORT), reload=RELOAD)
